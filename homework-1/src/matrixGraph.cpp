bb6474530c8d710c9d43b44c76d89c79deef2297
Andrew <andrewmosk@icloud.com>
2021-03-08 15:41:41 +0300

    Реализация MatrixGraph

---
+++ homework-1/src/matrixGraph.cpp
@@ -0,0 +1,82 @@
+//
+// Created by Андрей Москалёв on 08.03.2021.
+//
+#include "matrixGraph.h"
+
+template<typename T>
+int MatrixGraph<T>::VerticesCount() const {
+    return _vertices.size();
+}
+template<typename T>
+void MatrixGraph<T>::AddEdge(int from, int to, T &&element) {
+    _vertices.insert(from);
+    _vertices.insert(to);
+    if (int need_size = std::max(from, to); need_size >= _g.size()) {
+        need_size += 1;
+        _g.resize(need_size);
+        _gt.resize(need_size);
+        for (size_t i = 0; i < need_size; ++i) {
+            _g[i].resize(need_size);
+            _gt[i].resize(need_size);
+        }
+    }
+    _g[from][to].first = true;
+    _g[from][to].second = element;
+    _gt[to][from].first = true;
+    _gt[to][from].second = element;
+}
+template<typename T>
+void MatrixGraph<T>::GetNextVertices(int vertex, std::vector<int> &vertices) const {
+    assert(vertex < _g.size());
+    for (int to : _vertices) {
+        if (_g[vertex][to].first) {
+            vertices.push_back(to);
+        }
+    }
+}
+
+template<typename T>
+void MatrixGraph<T>::GetPrevVertices(int vertex, std::vector<int> &vertices) const {
+    assert(vertex < _g.size());
+    for (int to : _vertices) {
+        if (_gt[vertex][to].first) {
+            vertices.push_back(to);
+        }
+    }
+}
+
+template<typename T>
+void MatrixGraph<T>::DeepFirstSearch(int vertex, std::vector<int> &vertices) const {
+    assert(vertex < _g.size());
+    std::unordered_set<int> used;
+    _dfs(vertex, used, vertices);
+}
+
+template<typename T>
+void MatrixGraph<T>::_dfs(int vertex, std::unordered_set<int> used, std::vector<int> &vertices) const {
+    used.insert(vertex);
+    vertices.push_back(vertex);
+    for (int to : _vertices) {
+        if (_g[vertex][to].first && !used.contains(to)) {
+            _dfs(to, used, vertices);
+        }
+    }
+}
+
+template<typename T>
+void MatrixGraph<T>::BreadthFirstSearch(int vertex, std::vector<int> &vertices) const {
+    assert(vertex < _g.size());
+    std::queue<int> q;
+    q.push(vertex);
+    std::unordered_set<int> used;
+    while (!q.empty()) {
+        int cur_vertex = q.front();
+        q.pop();
+        for (int to : _vertices) {
+            if (_g[vertex][to].first && !used.contains(to)) {
+                vertices.push_back(cur_vertex);
+                used.insert(cur_vertex);
+            }
+        }
+    }
+}
--- homework-1/src/matrixGraph.h
+++ homework-1/src/matrixGraph.h
@@ -4,25 +4,36 @@
 
 
 #include "../graph.h"
+#include <vector>
+#include <unordered_set>
+#include <cassert>
+#include <queue>
 
 template<typename T = void>
 class MatrixGraph : public IGraph<T> {
 public:
-    virtual void AddEdge(int from, int to, T &&element) {};
+    virtual void AddEdge(int from, int to, T &&element);
 
-    MatrixGraph() {};
+    MatrixGraph() = default;
 
     MatrixGraph(IGraph<T> *_oth) {};
 
-    virtual int VerticesCount() const { return 0; };
+    [[nodiscard]] virtual int VerticesCount() const;
 
-    virtual void GetNextVertices(int vertex, std::vector<int> &vertices) const {};
+    virtual void GetNextVertices(int vertex, std::vector<int> &vertices) const;
 
-    virtual void GetPrevVertices(int vertex, std::vector<int> &vertices) const {};
+    virtual void GetPrevVertices(int vertex, std::vector<int> &vertices) const;
 
-    virtual void DeepFirstSearch(int vertex, std::vector<int> &vertices) const {};
+    virtual void DeepFirstSearch(int vertex, std::vector<int> &vertices) const;
 
-    virtual void BreadthFirstSearch(int vertex, std::vector<int> &vertices) const {};
+    virtual void BreadthFirstSearch(int vertex, std::vector<int> &vertices) const;
+protected:
+    std::vector<std::vector<std::pair<bool, T>>> _g;
+    std::vector<std::vector<std::pair<bool, T>>> _gt;
+
+    std::unordered_set<int> _vertices;
+
+    void _dfs(int vertex, std::unordered_set<int> used, std::vector<int> &vertices) const;
 };
 
 #endif //HOMEWORK_1_MATRIXGRAPH_H
