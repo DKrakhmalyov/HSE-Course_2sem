de8392be26ce5e47b5ef6ec2e631193225c69f4e
Andrew <andrewmosk@icloud.com>
2021-03-08 13:52:30 +0300

    Реализация ListGraph

---
+++ homework-1/src/listGraph.cpp
@@ -0,0 +1,60 @@
+//
+// Created by Андрей Москалёв on 08.03.2021.
+//
+
+#include "listGraph.h"
+
+template<typename T>
+int ListGraph<T>::VerticesCount() const {
+    return _g.size();
+}
+
+template<typename T>
+void ListGraph<T>::AddEdge(int from, int to, T &&element) {
+    _g[from].push_back({to, element});
+    _gt[to].push_back({from, element});
+}
+
+template<typename T>
+void ListGraph<T>::GetNextVertices(int vertex, std::vector<int> &vertices) const {
+    vertices = _g[vertex];
+}
+
+template<typename T>
+void ListGraph<T>::GetPrevVertices(int vertex, std::vector<int> &vertices) const {
+    vertices = _gt[vertex];
+}
+
+template<typename T>
+void ListGraph<T>::DeepFirstSearch(int vertex, std::vector<int> &vertices) const {
+    std::unordered_set<int> used;
+    _dfs(vertex, used, vertices);
+}
+
+template<typename T>
+void ListGraph<T>::_dfs(int vertex, std::unordered_set<int> used, std::vector<int> &vertices) const {
+    used.insert(vertex);
+    vertices.push_back(vertex);
+    for (int to : _g[vertex]) {
+        if (!used.contains(to)) {
+            _dfs(to, used, vertices);
+        }
+    }
+}
+
+template<typename T>
+void ListGraph<T>::BreadthFirstSearch(int vertex, std::vector<int> &vertices) const {
+    std::queue<int> q;
+    q.push(vertex);
+    std::unordered_set<int> used;
+    while (!q.empty()) {
+        int cur_vertex = q.front();
+        q.pop();
+        for (int to : _g[cur_vertex]) {
+            if (!used.contains(to)) {
+                vertices.push_back(cur_vertex);
+                used.insert(cur_vertex);
+            }
+        }
+    }
+}
--- homework-1/src/listGraph.h
+++ homework-1/src/listGraph.h
@@ -3,27 +3,34 @@
 #define HOMEWORK_1_LISTGRAPH_H
 
 #include "../graph.h"
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
 
 
 template<typename T>
 class ListGraph : public IGraph<T> {
 public:
-    virtual void AddEdge(int from, int to, T &&element) {};
+    virtual void AddEdge(int from, int to, T &&element);
 
-    ListGraph() {};
+    ListGraph() = default;;
 
     ListGraph(IGraph<T> *_oth) {};
 
-    virtual int VerticesCount() const { return 0; };
+    [[nodiscard]] virtual int VerticesCount() const;
 
-    virtual void GetNextVertices(int vertex, std::vector<int> &vertices) const {};
+    virtual void GetNextVertices(int vertex, std::vector<int> &vertices) const;
 
-    virtual void GetPrevVertices(int vertex, std::vector<int> &vertices) const {};
+    virtual void GetPrevVertices(int vertex, std::vector<int> &vertices) const;
 
-    virtual void DeepFirstSearch(int vertex, std::vector<int> &vertices) const {};
+    virtual void DeepFirstSearch(int vertex, std::vector<int> &vertices) const;
 
-    virtual void BreadthFirstSearch(int vertex, std::vector<int> &vertices) const {};
+    virtual void BreadthFirstSearch(int vertex, std::vector<int> &vertices) const;
+private:
+    std::unordered_map<int, std::vector<std::pair<int, T>>> _g;
+    std::unordered_map<int, std::vector<std::pair<int, T>>> _gt;
+    void _dfs(int vertex, std::unordered_set<int> used, std::vector<int> &vertices) const;
 };
-
 
 #endif //HOMEWORK_1_LISTGRAPH_H
