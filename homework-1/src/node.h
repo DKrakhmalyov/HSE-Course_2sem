8c8458bffa0066df7a1441a9e12213366f9dca56
Andrew <andrewmosk@icloud.com>
2021-03-08 16:38:57 +0300

    реализация ptrsGraph

--- homework-1/src/node.h
+++ homework-1/src/node.h
@@ -1,9 +1,13 @@
 
 #ifndef HOMEWORK_1_NODE_H
 #define HOMEWORK_1_NODE_H
+#include <vector>
+#include <utility>
 
 template<typename T>
 class Node {
+    std::vector<std::pair<Node<T>*, T>> incoming;
+    std::vector<std::pair<Node<T>*, T>> outgoing;
 };
 
 #endif //HOMEWORK_1_NODE_H
