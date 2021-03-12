e62aa1b1ba77863c6fd306b83659416363297cbb
Denis <krakhmalev23@gmail.com>
2021-03-12 21:53:06 +0300

    Iterator signatures

--- homework-2/static_array.h
+++ homework-2/static_array.h
@@ -5,29 +5,50 @@
 #ifndef HOMEWORK_2_STATIC_ARRAY_H
 #define HOMEWORK_2_STATIC_ARRAY_H
 
-template <typename T, size_t sz = 0>
-class static_array{
+template<typename T, size_t sz = 0>
+class static_array {
 public:
-    class iterator;
+    class iterator {
+        iterator(const iterator &);
+
+        ~iterator();
+
+        iterator &operator=(const iterator &);
+
+        iterator &operator++();
+
+        iterator &operator--();
+
+        T *operator->() const;
+
+        T &operator*() const;
+
+        friend bool operator==(const iterator &, const iterator &);
+
+        friend bool operator!=(const iterator &, const iterator &);
+    };
 
     static_array();
+
     static_array(size_t sz);
 
     size_t current_size();
+
     size_t size();
 
     void clear();
 
-    static_array::iterator emplace(size_t ind, T&& obj);
+    static_array::iterator emplace(size_t ind, T &&obj);
 
-    template < class... Args >
-    static_array::iterator emplace(size_t ind, Args&&... args);
+    template<class... Args>
+    static_array::iterator emplace(size_t ind, Args &&... args);
 
     void erase(static_array::iterator);
 
-    T& at(size_t ind);
+    T &at(size_t ind);
 
     static_array::iterator begin();
+
     static_array::iterator end();
 };
 
