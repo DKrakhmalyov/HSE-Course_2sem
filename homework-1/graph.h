e0e2c2b9ba1dcbf20b1c6db26337397ad295a8f4
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2021-03-20 22:19:00 +0300

    Update graph.h

--- homework-1/graph.h
+++ homework-1/graph.h
@@ -1,50 +1,38 @@
-
 #ifndef HOMEWORK_1_GRAPH_H
 #define HOMEWORK_1_GRAPH_H
-
 #include <vector>
 #include "src/node.h"
-
 template<typename T>
 class IGraph {
 public:
     virtual ~IGraph() {}
-
     IGraph() {};
-
     IGraph(IGraph *_oth) {};
-
     virtual void AddEdge(int from, int to, T &&_obj) = 0;
-
     virtual int VerticesCount() const = 0;
-
     virtual void GetNextVertices(int vertex, std::vector<int> &vertices) const = 0;
-
     virtual void GetPrevVertices(int vertex, std::vector<int> &vertices) const = 0;
-
     virtual void DeepFirstSearch(int vertex, std::vector<int> &vertices) const = 0;
 
     virtual void BreadthFirstSearch(int vertex, std::vector<int> &vertices) const = 0;
+
+    virtual T GetWeight(int from, int to) const = 0;
+
+    virtual std::vector<int> GetAllVertices() const = 0;
+
+    
 };
 
 template<typename T = void>
 class IPtrsGraph {
 public:
     virtual ~IPtrsGraph() {}
-
     IPtrsGraph() {};
-
     virtual void AddEdge(Node<T> *from, Node<T> *to, T &&_obj) = 0;
-
     virtual int VerticesCount() const = 0;
-
     virtual void GetNextVertices(Node<T> *vertex, std::vector<Node<T> *> &vertices) const = 0;
-
     virtual void GetPrevVertices(Node<T> *vertex, std::vector<Node<T> *> &vertices) const = 0;
-
     virtual void DeepFirstSearch(Node<T> *vertex, std::vector<Node<T> *> &vertices) const = 0;
-
     virtual void BreadthFirstSearch(Node<T> *vertex, std::vector<Node<T> *> &vertices) const = 0;
 };
-
 #endif //HOMEWORK_1_GRAPH_H
