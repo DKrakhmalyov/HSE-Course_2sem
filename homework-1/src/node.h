3741937ae07c4d7abe72ce70f0595b8a08aa0d19
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2021-03-20 22:14:42 +0300

    Update node.h

--- homework-1/src/node.h
+++ homework-1/src/node.h
@@ -1,9 +1,14 @@
-
 #ifndef HOMEWORK_1_NODE_H
 #define HOMEWORK_1_NODE_H
 
+#include <vector>
+#include <utility>
+
+
 template<typename T>
-class Node {
+struct Node {
+    std::vector<std::pair<Node *, T>> nodes;
 };
 
 #endif //HOMEWORK_1_NODE_H
+
