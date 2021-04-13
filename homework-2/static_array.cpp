f35115b0eca3f73404978e18855acae9a2988a5b
chap4ev <sergey.02.nesterenko@gmail.com>
2021-04-14 02:05:57 +0300

    init hw2

--- homework-2/static_array.cpp
+++ homework-2/static_array.cpp
@@ -2,5 +2,104 @@
 // Created by Denis on 12.03.2021.
 //
 
-#include "static_array.h"
+// #include <stdexcept>
+// #include <memory>
+// #include <vector>
 
+// #include "static_array.h"
+
+
+// template<typename T, size_t sz>
+// static_array<T, sz>::static_array() : static_array(sz) { };
+
+// template<typename T, size_t sz>
+// static_array<T, sz>::static_array(size_t copacity) : _copacity(copacity) {
+//   _data = new char[copacity * sizeof(T)];
+//   _initialized.resize(copacity);
+// };
+
+// template<typename T, size_t sz>
+// size_t static_array<T, sz>::current_size() {
+//   return _current_size;
+// };
+
+// template<typename T, size_t sz>
+// size_t static_array<T, sz>::size() {
+//   return _copacity;
+// };
+
+// template<typename T, size_t sz>
+// void static_array<T, sz>::clear() {
+//   for (auto i = begin(); i != end(); ++i)
+//     erase(i);
+// };
+
+// template<typename T, size_t sz>
+// static_array::iterator static_array<T, sz>::emplace(size_t ind, T &&obj) {
+//   _check_bounds(ind);
+//   if (_initialized[ind]) {
+//     reinterpret_cast<T*>(_data + sizeof(T)*ind)->~T();
+//     --_current_size;
+//   }
+//   new (_data + sizeof(T)*ind) T(std::forward<T>(obj));
+//   _initialized[ind] = true;
+//   ++_current_size;
+//   return static_array::iterator(ind);
+// };
+
+// template<typename T, size_t sz, class... Args>
+// static_array::iterator static_array<T, sz>::emplace(size_t ind, Args &&... args) {
+//   check_bounds_(ind);
+//   if (_initialized[ind]) {
+//     reinterpret_cast<T*>(_data + sizeof(T)*ind)->~T();
+//     --_current_size;
+//   }
+//   new (_data + sizeof(T)*ind) T(std::forward<Args>(args)...);
+//   _initialized[ind] = true;
+//   ++_current_size;
+// };
+
+// template<typename T, size_t sz>
+// void static_array<T, sz>::erase(static_array::iterator) {
+
+// };
+
+// template<typename T, size_t sz>
+// T& static_array<T, sz>::at(size_t ind) {
+//   _check_bounds(ind);
+//   _check_is_initialized(ind);
+//   return *reinterpret_cast<T*>(_data + sizeof(T) * ind);
+// };
+
+// template<typename T, size_t sz>
+// static_array::iterator static_array<T, sz>::begin() {
+//   return static_array::iterator(
+//       reinterpret_cast<T*>(_data + sizeof(T) * _get_mext_avaliable_index(-1))
+//     );
+// };
+
+// template<typename T, size_t sz>
+// static_array::iterator static_array<T, sz>::end() {
+//   return static_array::iterator(reinterpret_cast<T*>(_data + sizeof(T) * _copacity));
+// };
+
+
+// template<typename T, size_t sz>
+// void static_array<T, sz>::_check_bounds(size_t& ind) {
+//   if (ind >= _copacity)
+//     throw std::out_of_range("static_array: Index out of range");
+// };
+
+// template<typename T, size_t sz>
+// void static_array<T, sz>::_check_is_initialized(size_t& ind) {
+//   if (!_initialized[ind])
+//     throw std::runtime_error("static_array: Value is not initialized");
+// }
+
+// template<typename T, size_t sz>
+// size_t static_array<T, sz>::_get_mext_avaliable_index(size_t index) {
+//   for (++index; index < _copacity; ++index)
+//     if (_initialized[index]) 
+//       break;
+//   return index;
+// }
--- homework-2/static_array.h
+++ homework-2/static_array.h
@@ -1,56 +1,145 @@
 //
-// Created by Denis on 12.03.2021.
+// govnocode created by chap4ev on 14.04.2021
 //
 
 #ifndef HOMEWORK_2_STATIC_ARRAY_H
 #define HOMEWORK_2_STATIC_ARRAY_H
 
+#include <stdexcept>
+#include <memory>
+#include <vector>
+
 template<typename T, size_t sz = 0>
 class static_array {
-public:
-    class iterator {
-    public:
-        iterator(const iterator &);
+ public:
+  class iterator {
+   protected:
+    friend class static_array;
+    T* _item;
 
-        ~iterator();
+    iterator(T* item) : _item(item) { };
 
-        iterator &operator=(const iterator &);
+   public:
+    iterator(const iterator& other) : _item(other._item) { };
 
-        iterator &operator++();
+    ~iterator() = default;
 
-        iterator &operator--();
-
-        T *operator->() const;
-
-        T &operator*() const;
-
-        friend bool operator==(const iterator &, const iterator &);
-
-        friend bool operator!=(const iterator &, const iterator &);
+    iterator& operator=(const iterator& other) {
+      _item = other._item;
+      return *this;
     };
 
-    static_array();
+    iterator& operator++() {
+      _item = reinterpret_cast<T*>(_data) + _get_mext_avaliable_index(_item - _data);
+      return *this;
+    };
 
-    static_array(size_t sz);
+    iterator& operator--() {
+      return *this;
+    };
 
-    size_t current_size();
+    T* operator->() const {
+      return _item;
+    };
 
-    size_t size();
+    T& operator*() const {
+      return *_item;
+    };
 
-    void clear();
+    friend bool operator==(const iterator& lhs, const iterator& rhs) {
+      return lhs._item == rhs._item;
+    };
 
-    static_array::iterator emplace(size_t ind, T &&obj);
+    friend bool operator!=(const iterator& lhs, const iterator& rhs) {
+      return !(lhs == rhs);
+    };
+  };
 
-    template<class... Args>
-    static_array::iterator emplace(size_t ind, Args &&... args);
+  static_array() : static_array(sz) { };
 
-    void erase(static_array::iterator);
+  explicit static_array(size_t copacity) : _copacity(copacity) {
+    _data = new char[copacity * sizeof(T)];
+    _initialized.resize(copacity);
+  };
 
-    T &at(size_t ind);
+  size_t current_size() {
+    return _current_size;
+  };
 
-    static_array::iterator begin();
+  size_t size() {
+    return _copacity;
+  };
 
-    static_array::iterator end();
+  void clear() {
+    for (auto i = begin(); i != end(); ++i)
+      erase(i);
+  };
+
+  static_array::iterator emplace(size_t ind, T &&obj) {
+    _check_bounds(ind);
+    if (_initialized[ind]) {
+      reinterpret_cast<T*>(_data + sizeof(T) * ind)->~T();
+      --_current_size;
+    }
+    new (_data + sizeof(T) * ind) T(std::forward<T>(obj));
+    _initialized[ind] = true;
+    ++_current_size;
+    return static_array::iterator(reinterpret_cast<T*>(_data + sizeof(T) * ind));
+  };
+
+  template<class... Args>
+  static_array::iterator emplace(size_t ind, Args &&... args) {
+    _check_bounds(ind);
+    if (_initialized[ind]) {
+      reinterpret_cast<T*>(_data + sizeof(T) * ind)->~T();
+      --_current_size;
+    }
+    new (_data + sizeof(T) * ind) T(std::forward<Args>(args)...);
+    _initialized[ind] = true;
+    ++_current_size;
+    return static_array::iterator(reinterpret_cast<T*>(_data + sizeof(T) * ind));
+  };
+
+  void erase(static_array::iterator) {
+
+  };
+
+  T& at(size_t ind) {
+    _check_bounds(ind);
+    _check_is_initialized(ind);
+    return *reinterpret_cast<T*>(_data + sizeof(T) * ind);
+  };
+
+  static_array::iterator begin() {
+    return static_array::iterator(
+             reinterpret_cast<T*>(_data + sizeof(T) * _get_mext_avaliable_index(-1))
+           );
+  };
+
+  static_array::iterator end() {
+    return static_array::iterator(reinterpret_cast<T*>(_data + sizeof(T) * _copacity));
+  };
+
+ protected:
+  char* _data;
+  std::vector<bool> _initialized;
+  size_t _copacity;
+  size_t _current_size;
+
+  void _check_bounds(size_t& ind) {
+    if (ind >= _copacity)
+      throw std::out_of_range("static_array: Index out of range");
+  };
+  void _check_is_initialized(size_t& ind) {
+    if (!_initialized[ind])
+      throw std::runtime_error("static_array: Value is not initialized");
+  }
+  size_t _get_mext_avaliable_index(size_t index) {
+    for (++index; index < _copacity; ++index)
+      if (_initialized[index])
+        break;
+    return index;
+  }
 };
 
 #endif //HOMEWORK_2_STATIC_ARRAY_H
