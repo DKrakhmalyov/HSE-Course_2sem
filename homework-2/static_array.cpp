b337e1509cacd954a86c4f277ccd7065be5e5bf3
Danverr <79198736000@mail.ru>
2021-04-13 16:33:44 +0300

    Homework-2 completed

--- homework-2/static_array.cpp
+++ homework-2/static_array.cpp
@@ -4,3 +4,151 @@
 
 #include "static_array.h"
 
+
+/*
+template<typename T, size_t sz>
+static_array<T, sz>::iterator::iterator(const static_array<T, sz>::iterator& it) {
+    this = it;
+}
+
+template<typename T, size_t sz>
+static_array<T, sz>::iterator::~iterator() {}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator& static_array<T, sz>::iterator::operator=(const static_array<T, sz>::iterator& it) {
+    _value = it->value;
+    _next = it->next;
+    _prev = it->prev;
+}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator& static_array<T, sz>::iterator::operator++() {
+    do {
+        this = this->_next;
+    } while (!this->value);
+}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator& static_array<T, sz>::iterator::operator--() {
+    do {
+        this = this->_prev;
+    } while (!this->value);
+}
+
+template<typename T, size_t sz>
+T* static_array<T, sz>::iterator::operator->() const {
+    return _value.get();
+}
+
+template<typename T, size_t sz>
+T& static_array<T, sz>::iterator::operator*() const {
+    return *_value.get();
+}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator operator+(typename static_array<T, sz>::iterator a, int b) {
+    for (int i = b; i < b; i++) a++;
+    return a;
+}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator operator-(typename static_array<T, sz>::iterator a, int b) {
+    for (int i = b; i < b; i++) a--;
+    return a;
+}
+
+template<typename T, size_t sz>
+bool operator==(const typename static_array<T, sz>::iterator& a, const typename static_array<T, sz>::iterator& b) {
+    return a->value == b->value;
+}
+
+template<typename T, size_t sz>
+bool operator!=(const typename static_array<T, sz>::iterator& a, const typename static_array<T, sz>::iterator& b) {
+    return !(a == b);
+}
+
+
+
+template<typename T, size_t sz>
+static_array<T, sz>::static_array() {
+    auto prev_it = _begin;
+
+    for (int i = 0; i < _size; i++) {
+        auto it = iterator();
+        it->value = make_shared<T>(new T());
+
+        if (i == 0) {
+            _begin = it;
+        } else {
+            it->_prev = prev_it;
+            prev_it->_next = it;
+        }
+
+        if (i == _size - 1) {
+            it->_next = _end;
+            _end->_prev = it;
+        }
+    }
+}
+
+template<typename T, size_t sz>
+static_array<T, sz>::static_array(size_t size) {
+    clear();
+    _size = size;
+    static_array();
+}
+
+template<typename T, size_t sz>
+size_t static_array<T, sz>::current_size() {
+    return _current_size;
+}
+
+template<typename T, size_t sz>
+size_t static_array<T, sz>::size() {
+    return _size;
+}
+
+template<typename T, size_t sz>
+void static_array<T, sz>::clear() {
+    for (auto it = _begin; it != _end; it++) {
+        erase(it);
+    }
+}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator static_array<T, sz>::emplace(size_t ind, T&& obj) {
+    auto it = _begin + ind;
+    it->value = obj;
+    _current_size++;
+    return it;
+}
+
+template<typename T, size_t sz>
+template<class... Args>
+typename static_array<T, sz>::iterator static_array<T, sz>::emplace(size_t ind, Args &&... args) {
+    return emplace(ind, new T(args...));
+}
+
+template<typename T, size_t sz>
+void static_array<T, sz>::erase(static_array<T, sz>::iterator it) {
+    it->value = nullptr;
+    _current_size--;
+}
+
+template<typename T, size_t sz>
+T& static_array<T, sz>::at(size_t ind) {
+    if (ind >= _size) throw std::out_of_range("Given index is out of static_array range");
+    return _begin + ind;
+}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator static_array<T, sz>::begin() {
+    return _begin;
+}
+
+template<typename T, size_t sz>
+typename static_array<T, sz>::iterator static_array<T, sz>::end() {
+    return _end;
+}
+
+*/
--- homework-2/static_array.h
+++ homework-2/static_array.h
@@ -5,52 +5,179 @@
 #ifndef HOMEWORK_2_STATIC_ARRAY_H
 #define HOMEWORK_2_STATIC_ARRAY_H
 
+#include <stddef.h>
+#include <vector>
+#include <stdexcept>
+
 template<typename T, size_t sz = 0>
 class static_array {
 public:
     class iterator {
     public:
-        iterator(const iterator &);
+        friend class static_array;
 
-        ~iterator();
+        iterator() {}
 
-        iterator &operator=(const iterator &);
+        iterator(const iterator& it) {
+            *this = it;
+        }
 
-        iterator &operator++();
+        ~iterator() {};
 
-        iterator &operator--();
+        bool is_null() {
+            return (_index == -1 || _is_null == nullptr) ? true : (*_is_null)[_index];
+        }
 
-        T *operator->() const;
+        iterator& operator=(const iterator& it) {
+            _value = it._value;
+            _index = it._index;
+            _next = it._next;
+            _prev = it._prev;
+            return *this;
+        }
 
-        T &operator*() const;
+        iterator& operator++() {
+            do {
+                *this = *this->_next;
+            } while (this->is_null() && this->_next != nullptr);
+            return *this;
+        }
 
-        friend bool operator==(const iterator &, const iterator &);
+        iterator& operator--() {
+            do {
+                *this = *this->_prev;
+            } while (this->is_null() && this->_prev != nullptr);
+            return *this;
+        }
 
-        friend bool operator!=(const iterator &, const iterator &);
+        T* operator->() const {
+            return _value;
+        };
+
+        T& operator*() const {
+            return *_value;
+        };
+
+        friend bool operator==(const iterator& a, const iterator& b) {
+            return a._index == b._index;
+        };
+
+        friend bool operator!=(const iterator& a, const iterator& b) {
+            return !(a == b);
+        };
+
+    private:
+        int _index = -1;
+        std::vector<bool>* _is_null = nullptr;
+        T* _value = nullptr;
+        iterator* _next = nullptr;
+        iterator* _prev = nullptr;
     };
 
-    static_array();
+    static_array() : static_array(sz) {};
 
-    static_array(size_t sz);
+    static_array(size_t size) {
+        _is_null.assign(size, true);
+        iterator* prev_it = nullptr;
+        _end = new iterator();
 
-    size_t current_size();
+        for (int i = 0; i < size; i++) {
+            auto it = new iterator();
+            it->_index = i;
+            it->_is_null = &_is_null;
 
-    size_t size();
+            if (i == size - 1) {
+                it->_next = _end;
+                _end->_prev = it;
+            }
 
-    void clear();
+            if (i == 0) {
+                _begin = it;
+            } else {
+                it->_prev = prev_it;
+                prev_it->_next = it;
+            }
 
-    static_array::iterator emplace(size_t ind, T &&obj);
+            prev_it = it;
+        }
+    };
+
+    ~static_array() {
+        auto it = _begin;
+
+        while (it->_next != nullptr) {
+            erase(*it);
+            auto prev = it;
+            it = it->_next;
+            delete prev;
+        }
+
+        delete it;
+    }
+
+    size_t current_size() {
+        return _current_size;
+    };
+
+    size_t size() {
+        return _is_null.size();
+    };
+
+    void clear() {
+        for (auto it = _begin; it->_next != nullptr; it = it->_next) {
+            erase(*it);
+        }
+    };
+
+    static_array::iterator emplace(size_t ind, T&& obj) {
+        auto it = _begin;
+        for (int i = 0; i < ind; i++) it = it->_next;
+        it->_value = new T(std::forward<T>(obj));
+
+        if (_is_null[ind]) {
+            _current_size++;
+            _is_null[ind] = false;
+        }
+
+        return *it;
+    };
 
     template<class... Args>
-    static_array::iterator emplace(size_t ind, Args &&... args);
+    static_array::iterator emplace(size_t ind, Args&&... args) {
+        return emplace(ind, T(std::forward<Args>(args)...));
+    };
 
-    void erase(static_array::iterator);
+    void erase(static_array::iterator it) {
+        if (!_is_null[it._index]) {
+            _current_size--;
+            delete it._value;
+        }
+        _is_null[it._index] = true;
+    };
 
-    T &at(size_t ind);
+    T& at(size_t ind) {
+        if (ind >= _is_null.size()) {
+            throw std::out_of_range("Given index is out of static_array range");
+        }
 
-    static_array::iterator begin();
+        auto it = _begin;
+        for (int i = 0; i < ind; i++) it = it->_next;
+        return **it;
+    };
 
-    static_array::iterator end();
+    static_array::iterator begin() {
+        return *_begin;
+    };
+
+    static_array::iterator end() {
+        return *_end;
+    };
+
+private:
+    size_t _current_size = 0;
+    std::vector<bool> _is_null;
+    iterator* _begin = nullptr;
+    iterator* _end = nullptr;
 };
 
 #endif //HOMEWORK_2_STATIC_ARRAY_H
