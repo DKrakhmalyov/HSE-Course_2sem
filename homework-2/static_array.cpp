bba668c5057f4a3b1370c7c8f4e5d9d4d79a8419
JUSSIAR <klokov.stas.022@mail.ru>
2021-04-17 22:31:30 +0300

    First working version

--- homework-2/static_array.cpp
+++ homework-2/static_array.cpp
@@ -1,6 +1,194 @@
-//
-// Created by Denis on 12.03.2021.
-//
-
 #include "static_array.h"
 
+template <typename T, std::size_t sz>
+static_array<T, sz>::iterator::iterator(const static_array<T, sz>::iterator& it) {
+    curr_iter = it.curr_iter;
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::iterator::~iterator() {
+    curr_iter = nullptr;
+}
+
+template <typename T, std::size_t sz>
+typename static_array<T, sz>::iterator& static_array<T, sz>::iterator::operator=(
+    const static_array<T, sz>::iterator &other
+) {
+    if (this != &other) {
+        curr_iter = other.curr_iter;
+        begin_iter = other.begin_iter;
+        end_iter = other.end_iter;
+    }
+    return *this;
+ }
+
+template <typename T, std::size_t sz>
+typename static_array<T, sz>::iterator& static_array<T, sz>::iterator::operator++() {
+    bool first_step_flag = true;
+    while ((curr_iter != end_iter) && ((*curr_iter == nullptr) || first_step_flag)) {
+        ++curr_iter;
+        first_step_flag = false;
+    }
+    return *this;
+}   
+
+template <typename T, std::size_t sz>
+typename static_array<T, sz>::iterator& static_array<T, sz>::iterator::operator--() {
+    bool first_step_flag = true;
+    while ((curr_iter != begin_iter) && ((*curr_iter == nullptr) || first_step_flag)) {
+        --curr_iter;
+        first_step_flag = false;
+    }
+    return *this;
+}
+
+template <typename T, std::size_t sz>
+T* static_array<T, sz>::iterator::operator->() {
+    return *this->curr_iter;
+}
+
+template<typename T, std::size_t sz>
+T& static_array<T, sz>::iterator::operator*() {
+    return **this->curr_iter;
+}
+
+template <typename T, std::size_t sz >
+bool static_array<T, sz>::iterator::operator==(
+    const static_array<T, sz>::iterator &other
+) {
+    return curr_iter == other.curr_iter;
+}
+
+template <typename T, std::size_t sz>
+bool static_array<T, sz>::iterator::operator!=(
+    const static_array<T, sz>::iterator &other
+) {
+    return !operator==(other);
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::iterator::iterator(T** _curr_iter, T** _begin_iter, T** _end_iter) :
+    curr_iter(_curr_iter),
+    begin_iter(_begin_iter),
+    end_iter(_end_iter)
+{}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::static_array() {
+    total_count = sz;
+    elements_count = 0;
+    init_array();
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::static_array(std::size_t size) {
+    total_count = std::max(size, sz);
+    elements_count = 0;
+    init_array();
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::~static_array() {
+    clear();
+}
+
+template<typename T, std::size_t sz>
+void static_array<T, sz>::static_array::clear() {
+    delete_elements();
+    delete[] array;
+    array = nullptr;
+    total_count = 0;
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::iterator static_array<T, sz>::static_array::emplace(size_t pos, T &&obj) {
+    if (*(array + pos) == nullptr) {
+        elements_count++;
+    } else {
+        delete array[pos];
+    }
+    array[pos] = new T(std::forward<T>(obj));
+    return create_iter(pos);
+}
+
+template <typename T, std::size_t sz>
+template<class... Args>
+static_array<T, sz>::iterator static_array<T, sz>::static_array::emplace(size_t pos, Args &&... args) {
+    if (*(array + pos) == nullptr) {
+        elements_count++;
+    } else {
+        delete array[pos];
+    }
+    array[pos] = new T(std::forward<Args>(args)...);
+    return create_iter(pos);
+}
+
+template <typename T, std::size_t sz>
+void static_array<T, sz>::static_array::erase(static_array<T, sz>::iterator iter) {
+    if (*iter.curr_iter != nullptr) {
+        delete *iter.curr_iter;
+        --elements_count;
+        *iter.curr_iter = nullptr;
+    }
+}
+
+template <typename T, std::size_t sz>
+T& static_array<T, sz>::static_array::at(std::size_t pos) {
+    if (pos >= total_count) {
+        throw std::out_of_range(
+            "You tried to use element on position " + std::to_string(pos) + 
+            " but there is only " + std::to_string(total_count) +
+            " elements in array"
+        );
+    }
+    if (array[pos] == nullptr) {
+        throw std::invalid_argument(
+            "You tried to use non-existent element"
+        );
+    }
+    return *array[pos];
+}
+
+template <typename T, std::size_t sz>
+std::size_t static_array<T, sz>::static_array::current_size() const {
+    return elements_count;
+}
+
+template <typename T, std::size_t sz>
+std::size_t static_array<T, sz>::static_array::size() const {
+    return total_count;
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::iterator static_array<T, sz>::static_array::begin() {
+    return create_iter(0);
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::iterator static_array<T, sz>::static_array::end() {
+    return create_iter(total_count);
+}
+
+template <typename T, std::size_t sz>
+void static_array<T, sz>::static_array::init_array() {
+    array = new T*[total_count + 1];
+    for (std::size_t pos = 0; pos < total_count; ++pos) {
+        array[pos] = nullptr;
+    }
+}
+
+template <typename T, std::size_t sz>
+void static_array<T, sz>::static_array::delete_elements() {
+    for (std::size_t pos = 0; pos < total_count; ++pos) {
+        if (*(array + pos) != nullptr) {
+            delete array[pos];
+            --elements_count;
+            array[pos] = nullptr;
+        }
+    }
+}
+
+template <typename T, std::size_t sz>
+static_array<T, sz>::iterator static_array<T, sz>::static_array::create_iter(std::size_t pos) {
+    return static_array<T, sz>::iterator(array + pos, array, array + total_count);
+}
--- homework-2/static_array.h
+++ homework-2/static_array.h
@@ -1,56 +1,91 @@
-//
-// Created by Denis on 12.03.2021.
-//
+#include <utility>
+#include <cstddef>
+#include <stdexcept>
+#include <string>
+#include <iostream>
 
 #ifndef HOMEWORK_2_STATIC_ARRAY_H
 #define HOMEWORK_2_STATIC_ARRAY_H
 
-template<typename T, size_t sz = 0>
+template <typename T, std::size_t sz = 0>
 class static_array {
-public:
-    class iterator {
+
     public:
-        iterator(const iterator &);
 
-        ~iterator();
+        class iterator {
 
-        iterator &operator=(const iterator &);
+            friend class static_array;
 
-        iterator &operator++();
+            public:
+            
+                iterator(const iterator &);
 
-        iterator &operator--();
+                ~iterator();
 
-        T *operator->() const;
+                iterator &operator=(const iterator &);
 
-        T &operator*() const;
+                iterator &operator++();
 
-        friend bool operator==(const iterator &, const iterator &);
+                iterator &operator--();
 
-        friend bool operator!=(const iterator &, const iterator &);
-    };
+                T *operator->();
 
-    static_array();
+                T &operator*();
 
-    static_array(size_t sz);
+                bool operator==(const iterator &);
+                
+                bool operator!=(const iterator &);
 
-    size_t current_size();
+            private:
 
-    size_t size();
+                iterator(T**, T**, T**);
 
-    void clear();
+                T **curr_iter;
 
-    static_array::iterator emplace(size_t ind, T &&obj);
+                T **begin_iter;
 
-    template<class... Args>
-    static_array::iterator emplace(size_t ind, Args &&... args);
+                T **end_iter;
+        };
 
-    void erase(static_array::iterator);
+        static_array();
 
-    T &at(size_t ind);
+        static_array(std::size_t size);
 
-    static_array::iterator begin();
+        ~static_array();
 
-    static_array::iterator end();
+        std::size_t current_size() const;
+
+        std::size_t size() const;
+
+        void clear();
+
+        static_array::iterator emplace(std::size_t pos, T &&obj);
+
+        template<class... Args>
+        static_array::iterator emplace(std::size_t pos, Args &&... args);
+
+        void erase(static_array::iterator);
+
+        T &at(std::size_t pos);
+
+        static_array::iterator begin();
+
+        static_array::iterator end();
+
+    private:
+
+        T **array;
+
+        std::size_t elements_count;
+
+        std::size_t total_count;
+
+        void init_array();
+
+        void delete_elements();
+
+        static_array::iterator create_iter(std::size_t pos);
+
 };
 
 #endif //HOMEWORK_2_STATIC_ARRAY_H
