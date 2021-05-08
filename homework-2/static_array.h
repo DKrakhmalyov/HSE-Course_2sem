7bfd8fedbe96209ff0d2000e4433fa330d7ac5a0
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2021-05-08 21:37:09 +0300

    Update static_array.h

--- homework-2/static_array.h
+++ homework-2/static_array.h
@@ -9,6 +9,7 @@ template<typename T, size_t sz = 0>
 class static_array {
 public:
     class iterator {
+      public:
         iterator(const iterator &);
 
         ~iterator();
@@ -23,9 +24,16 @@ public:
 
         T &operator*() const;
 
-        friend bool operator==(const iterator &, const iterator &);
+         bool operator==(iterator const &other) const; 
 
-        friend bool operator!=(const iterator &, const iterator &);
+         bool operator!=(iterator const &other) const;
+
+         friend class static_array;
+    private:
+    iterator(static_array<T, sz> *array_);
+    iterator(static_array<T, sz> *array_, size_t pos_);
+    std::size_t pos;
+    static_array<T, sz> *array;
     };
 
     static_array();
@@ -43,13 +51,20 @@ public:
     template<class... Args>
     static_array::iterator emplace(size_t ind, Args &&... args);
 
-    void erase(static_array::iterator);
+    void erase(static_array::iterator it);
 
     T &at(size_t ind);
 
     static_array::iterator begin();
 
     static_array::iterator end();
+    private:
+    std::vector<T *>data;
+    std::vector<bool>used;
+    size_t cur_size_;
+    size_t size_;
+    static_array::iterator begin_;
+    static_array::iterator end_;
 };
 
 #endif //HOMEWORK_2_STATIC_ARRAY_H
