9cf242b02748bc2e32b1c0d1ca23c4ef5af91cee
Ilya Perstenev <perst20@gmail.com>
2021-05-15 14:50:57 +0300

    move_if_noexcept.h

--- homework-3/type_traits/move_if_noexcept.h
+++ homework-3/type_traits/move_if_noexcept.h
@@ -9,13 +9,24 @@
 // conditional
 template<bool condition, typename T, typename F>
 struct conditional {
-    ...
+    using type = T;
+};
+
+template<typename T, typename F>
+struct conditional<false, T, F> {
+    using type = F;
 };
 
 // conditional - partial specialization
-...
 
 template<bool condition, typename T, typename F>
-using conditional_v = ...
+using conditional_v = typename conditional<condition, T, F>::type;
 
 // move_if_noexcept
+
+template< class T >
+constexpr typename conditional<
+        std::conjunction_v<std::negation<is_nothrow_move_constructible<T>>,  is_copy_constructible<T>>,
+        const T&,
+        T&&
+>::type move_if_noexcept(T& x) noexcept;
