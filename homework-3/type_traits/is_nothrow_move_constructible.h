20880fe8f1044f1424de21af95710823f63d3d03
chap4ev <sergey.02.nesterenko@gmail.com>
2021-05-17 23:20:20 +0300

    is_nothrow_constructible fix

--- homework-3/type_traits/is_nothrow_move_constructible.h
+++ homework-3/type_traits/is_nothrow_move_constructible.h
@@ -4,11 +4,18 @@
 
 #include "utility.h"
 
+template<typename S, typename T, typename... Args>
+struct is_nt_constructible_impl
+  : std::false_type { };
 
 template<typename T, typename... Args>
-struct is_nothrow_constructible {
-  static constexpr bool value = noexcept( T(declval<Args>()...) );
-};
+struct is_nt_constructible_impl<void_t<decltype( T(declval<Args>()...) )>, T, Args...>
+  : std::integral_constant<bool, noexcept(T(declval<Args>()...))> { };
+
+
+template<typename T, typename... Args>
+struct is_nothrow_constructible
+  : is_nt_constructible_impl<void, T, Args...> { };
 
 template<typename T, std::size_t N>
 struct is_nothrow_constructible<T[N]>
