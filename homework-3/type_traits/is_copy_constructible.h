e1b2e9f6156394d1e22611ae38a97adf5b12363d
Makar <45318415+Makarkarius@users.noreply.github.com>
2021-05-21 18:48:21 +0300

    HW3

--- homework-3/type_traits/is_copy_constructible.h
+++ homework-3/type_traits/is_copy_constructible.h
@@ -5,37 +5,23 @@
 #include "utility.h"
 
 template<typename T, typename... Args>
-struct is_constructible_impl;
-
-template<typename Derived, typename Base>
-struct is_invalid_base_to_derived_cast {
-   ...
-};
+struct is_constructible_impl : std::false_type {};
 
 template<typename To, typename From>
-struct is_invalid_lvalue_to_rvalue_cast : std::false_type {
-    ...
-};
+struct is_invalid_lvalue_to_rvalue_cast : std::false_type {};
 
 template<typename RefTo, typename RefFrom>
-struct is_invalid_lvalue_to_rvalue_cast<RefTo&&, RefFrom&> {
-    ...
-};
-
-struct is_constructible_helper {
-    ...
-};
+struct is_invalid_lvalue_to_rvalue_cast<RefTo&&, RefFrom&> : std::true_type {};
 
 template<typename T, typename... Args>
-struct is_constructible_impl {
-    ...
-};
-
-// is_constructible_impl - partial specializations
-...
+struct is_constructible_impl<void_t<decltype(T(std::declval<Args>()...))>, T, Args...> : std::true_type {};
 
 template<typename T, typename... Args>
-struct is_constructible {...};
+struct is_constructible {
+	constexpr static bool value = is_constructible_impl<void, T, Args...>::value && !is_invalid_lvalue_to_rvalue_cast<T, Args...>::value;
+};
 
 template<typename T>
-struct is_copy_constructible {...};
+struct is_copy_constructible {
+	constexpr static bool value = is_constructible<T, add_lvalue_reference_t<add_const_t<T>>>::value;
+};
--- homework-3/type_traits/move_if_noexcept.h
+++ homework-3/type_traits/move_if_noexcept.h
@@ -8,14 +8,25 @@
 
 // conditional
 template<bool condition, typename T, typename F>
-struct conditional {
-    ...
+struct conditional {};
+
+template<typename T, typename F>
+struct conditional<true, T, F> {
+	using type = T;
 };
 
-// conditional - partial specialization
-...
+template<typename T, typename F>
+struct conditional<false, T, F> {
+	using type = F;
+};
 
 template<bool condition, typename T, typename F>
-using conditional_v = ...
+using conditional_v = typename conditional<condition, T, F>::type;
 
 // move_if_noexcept
+template<typename T>
+decltype(auto) move_if_noexcept(T&& value) {
+	using _T = uncvref_t<T>;
+	using res = conditional_v<is_nothrow_move_constructible<_T>::value, add_rvalue_reference_t<_T>, add_lvalue_reference_t<_T>>;
+	return static_cast<res>(value);
+}
