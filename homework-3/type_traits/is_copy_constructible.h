f77649821de467aa8ceb8e877d0167aec36479d1
pmy101202 <mepechyonkin@edu.hse.ru>
2021-05-31 12:12:27 +0300

    Beginning
    Utility.h and is_copy_constructible.h

--- homework-3/type_traits/is_copy_constructible.h
+++ homework-3/type_traits/is_copy_constructible.h
@@ -4,38 +4,37 @@
 
 #include "utility.h"
 
-template<typename T, typename... Args>
-struct is_constructible_impl;
-
-template<typename Derived, typename Base>
-struct is_invalid_base_to_derived_cast {
-   ...
-};
-
-template<typename To, typename From>
-struct is_invalid_lvalue_to_rvalue_cast : std::false_type {
-    ...
-};
-
-template<typename RefTo, typename RefFrom>
-struct is_invalid_lvalue_to_rvalue_cast<RefTo&&, RefFrom&> {
-    ...
-};
-
-struct is_constructible_helper {
-    ...
+template<typename, typename T, typename... Args>
+struct is_constructible_impl{
+    static constexpr bool value = false;
 };
 
 template<typename T, typename... Args>
-struct is_constructible_impl {
-    ...
+struct is_constructible_impl<void<decltype(T(Args...))>, T, Args...>{
+    static constexpr bool value = true;
 };
 
-// is_constructible_impl - partial specializations
-...
-
 template<typename T, typename... Args>
-struct is_constructible {...};
+struct is_constructible<T,Args>{
+    static constexpr bool value = is_constructible_impl<void, T, Args...>::value;
+};
 
 template<typename T>
-struct is_copy_constructible {...};
+struct is_constructible<add_lvalue_reference_t<T>,T>{
+    static constexpr bool value = false;
+};
+
+template<typename T>
+struct is_constructible<add_lvalue_reference_t<T>,add_rvalue_reference_t<T>>{
+    static constexpr bool value = false;
+};
+
+template<typename T>
+struct is_constructible<add_rvalue_reference_t<T>,add_lvalue_reference_t<T>>{
+    static constexpr bool value = false;
+};
+
+template<typename T>
+struct is_copy_constructible {
+    using value = is_constructible<T,T>::value;
+};
--- homework-3/type_traits/utility.h
+++ homework-3/type_traits/utility.h
@@ -2,34 +2,79 @@
 
 #include <type_traits>
 
+namespace removers{
+
 template<typename T>
-struct uncvref {
-    ...
+struct rem_const{
+    using type = T;
 };
 
 template<typename T>
-using uncvref_t = ...;
+struct rem_const<const T>{
+    using type = T;
+};
+
+template<typename T>
+struct rem_const<volatile T>{
+    using type = T;
+};
+
+template<typename T>
+struct rem_const<const volatile T>{
+    using type = T;
+};
+
+template<typename T>
+using rem_const_t = rem_const<T>::type;
+
+template<typename T>
+struct rem_ref{
+    using type = T;
+};
+
+template<typename T>
+struct rem_ref<T&>{
+    using type = T;
+};
+
+template<typename T>
+struct rem_ref<T&&>{
+    using type = T;
+};
+
+template<typename T>
+using rem_ref_t = rem_ref<T>::type;
+
+}
+
+template<typename T>
+struct uncvref {
+    using type = removers::rem_ref_t<removers::rem_const_t<T>>;
+};
+
+template<typename T>
+using uncvref_t = uncvref<T>::type;
 
 template<typename T>
 struct add_const {
-    ...
+    using type = const T;
 };
 
 template<typename T>
-using add_const_t = ...;
+using add_const_t = typename add_const<T>::type;
 
 template<typename T>
 struct add_lvalue_reference {
-    ...
+    using type = T&;
 };
 
 template<typename T>
 struct add_rvalue_reference {
-    ...
+    using type = T&&;
 };
 
 template<typename T>
-using add_lvalue_reference_t = ...
+using add_lvalue_reference_t = typename add_lvalue_reference<t>::type;
 
 template<typename T>
-using add_rvalue_reference_t = ...
+using add_rvalue_reference_t = typename add_rvalue_reference<t>::type;
