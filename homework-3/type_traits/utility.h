be6eeedda0d44dc150e408614b54192d9829c127
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2021-05-31 18:49:18 +0300

    Update utility.h

--- homework-3/type_traits/utility.h
+++ homework-3/type_traits/utility.h
@@ -2,34 +2,60 @@
 
 #include <type_traits>
 
+
+template<typename T>
+struct remove_cv {
+	using type = T;
+};
+template<typename T>
+struct remove_cv <const T>{
+	using type = T;
+};
+template<typename T>
+struct remove_cv <volatile T>{
+	using type = T;
+};
+
+template<typename T>
+struct remove_cv <const volatile T> {
+	using type = T;
+};
+template<typename T>
+
+struct remove_cv_t {
+	using type = typename remove_cv<T>::type;
+};
+
+template<typename T>
+struct remove_reference {
+	using type = T;
+};
+template<typename T>
+struct remove_reference <T&> {
+	using type = T;
+};
+template<typename T>
+struct remove_reference <T&&> {
+	using type = T;
+};
+
+template<typename T>
+struct remove_reference_t{
+	using type = typename remove_reference<T>::type;
+};
 template<typename T>
 struct uncvref {
-    ...
+	using type = remove_reference_t<remove_cv_t<T>>;
 };
 
 template<typename T>
-using uncvref_t = ...;
+using uncvref_t = typename uncvref<T>::type;
 
 template<typename T>
-struct add_const {
-    ...
-};
+using add_const_t = const T;
 
 template<typename T>
-using add_const_t = ...;
+using add_lvalue_reference_t = T&;
 
 template<typename T>
-struct add_lvalue_reference {
-    ...
-};
-
-template<typename T>
-struct add_rvalue_reference {
-    ...
-};
-
-template<typename T>
-using add_lvalue_reference_t = ...
-
-template<typename T>
-using add_rvalue_reference_t = ...
+using add_rvalue_reference_t = T&&;
