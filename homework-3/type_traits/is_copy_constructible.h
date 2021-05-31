177bce71920bc0abd751b791780a1e74ca67a417
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2021-05-31 18:47:31 +0300

    Update is_copy_constructible.h

--- homework-3/type_traits/is_copy_constructible.h
+++ homework-3/type_traits/is_copy_constructible.h
@@ -4,38 +4,45 @@
 
 #include "utility.h"
 
-template<typename T, typename... Args>
-struct is_constructible_impl;
 
-template<typename Derived, typename Base>
-struct is_invalid_base_to_derived_cast {
-   ...
-};
 
 template<typename To, typename From>
 struct is_invalid_lvalue_to_rvalue_cast : std::false_type {
-    ...
+	constexpr static bool value = false;
 };
-
-template<typename RefTo, typename RefFrom>
-struct is_invalid_lvalue_to_rvalue_cast<RefTo&&, RefFrom&> {
-    ...
-};
-
-struct is_constructible_helper {
-    ...
-};
-
-template<typename T, typename... Args>
-struct is_constructible_impl {
-    ...
-};
-
-// is_constructible_impl - partial specializations
-...
-
-template<typename T, typename... Args>
-struct is_constructible {...};
 
 template<typename T>
-struct is_copy_constructible {...};
+struct is_invalid_lvalue_to_rvalue_cast <T&,T> {
+	constexpr static bool value = true;
+};
+
+template<typename T>
+struct is_invalid_lvalue_to_rvalue_cast <T&, T&&> {
+	constexpr static bool value = true;
+};
+template<typename T>
+struct is_invalid_lvalue_to_rvalue_cast <T&&,T&> {
+	constexpr static bool value = true;
+};
+
+template<typename ,typename T, typename... Args>
+struct is_constructible_impl
+{
+	constexpr static bool value = false;
+};
+
+template<typename T, typename... Args>
+struct is_constructible_impl <std::void_t<decltype(T(std::declval<Args>()...))>, T, Args...>
+{
+	constexpr static bool value = true;
+};
+
+template<typename T,typename... Args>
+struct is_constructible {
+	constexpr static bool value = !is_invalid_lvalue_to_rvalue_cast<T,Args...>::value && is_constructible_impl<std::void_t<>,T, Args...>::value;
+};
+
+template<typename T>
+struct is_copy_constructible { 
+	constexpr static bool value = is_constructable<T, T>::value;
+};
