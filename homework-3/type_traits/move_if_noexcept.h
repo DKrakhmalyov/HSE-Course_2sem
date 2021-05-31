a3c8488f6bc13653880ee50d1b847721aa179a32
Grif-cpp <71193593+Grif-cpp@users.noreply.github.com>
2021-05-31 18:48:53 +0300

    Update move_if_noexcept.h
    хотел написать move_if_noexcept через conditionals, но возникли проблемы с приведением типов, пришлось делать по-другому.

--- homework-3/type_traits/move_if_noexcept.h
+++ homework-3/type_traits/move_if_noexcept.h
@@ -5,17 +5,29 @@
 #include "is_copy_constructible.h"
 #include "is_nothrow_move_constructible.h"
 #include "utility.h"
+//template<bool condition, typename T, typename F>
+//struct conditional;
+//
+//template<typename T, typename F>
+//struct conditional<true, T, F>		
+//{
+//	using type = T;
+//};
+//
+//template<typename T, typename F>
+//struct conditional<false, T, F>
+//{
+//	using type = F;
+//};
+//
+//template<bool condition, typename T, typename F>
+//using conditional_t = typename conditional<condition, T, F>::type;
 
-// conditional
-template<bool condition, typename T, typename F>
-struct conditional {
-    ...
-};
-
-// conditional - partial specialization
-...
-
-template<bool condition, typename T, typename F>
-using conditional_v = ...
-
-// move_if_noexcept
+template<typename T>
+decltype(auto) move_if_noexcept(T&& value)
+{
+	if constexpr (!is_nothrow_move_constructible<std::decay_t<T>>::value)
+		return std::forward<T>(value);                                   //lvalue
+	else
+		return std::move(std::forward<T>(value));                        //rvalue
+}
