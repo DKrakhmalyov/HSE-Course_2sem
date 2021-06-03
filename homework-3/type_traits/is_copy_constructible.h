8496da1bcab092e5df0d30fed5e5492e6ab91e8a
c5xheavy <46067589+c5xheavy@users.noreply.github.com>
2021-06-04 01:39:23 +0300

    Add files via upload

--- homework-3/type_traits/is_copy_constructible.h
+++ homework-3/type_traits/is_copy_constructible.h
@@ -24,10 +24,12 @@ struct is_constructible<T, F> {
     using T_ = typename uncvref<T>::type;
     using F_ = typename uncvref<F>::type;
 
-    constexpr static bool value = is_constructible_impl<T, F>(0) && 
-        !(std::is_same_v<T_, F_> &&
-         (std::is_rvalue_reference_v<T> &&  std::is_lvalue_reference_v<F> ||
-          std::is_lvalue_reference_v<T> && !std::is_lvalue_reference_v<F>));
+    constexpr static bool lref_from_lref_ = std::is_lvalue_reference_v<T> && std::is_lvalue_reference_v<F>;
+    constexpr static bool rref_from_nonlref_ = std::is_rvalue_reference_v<T> && !std::is_lvalue_reference_v<F>;
+    constexpr static bool val_from_any_ = !std::is_lvalue_reference_v<T> && !std::is_rvalue_reference_v<T>;
+
+    constexpr static bool value = std::is_same_v<T_, F_> ? (lref_from_lref_ || rref_from_nonlref_ || val_from_any_) :
+        is_constructible_impl<T, F>(0);
 };
 
 template<typename T>
